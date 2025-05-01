29d7b155440725c3ff813962c034d36542b279c6
Alexandra <aleksawork.k@gmail.com>
2025-05-02 01:30:39 +0300

    Add project files.

---
+++ lab_6/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace lab_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaveKeys = new System.Windows.Forms.Button();
+            this.btnGenerateKeys = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecryptFile = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEncryptFile = new System.Windows.Forms.Button();
+            this.txtPublicKey = new System.Windows.Forms.TextBox();
+            this.txtPrivateKey = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblPublicKey = new System.Windows.Forms.Label();
+            this.вса = new System.Windows.Forms.Label();
+            this.lblPrivateKey = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSaveKeys
+            // 
+            this.btnSaveKeys.Location = new System.Drawing.Point(404, 250);
+            this.btnSaveKeys.Name = "btnSaveKeys";
+            this.btnSaveKeys.Size = new System.Drawing.Size(182, 23);
+            this.btnSaveKeys.TabIndex = 0;
+            this.btnSaveKeys.Text = "Зберегти ключі";
+            this.btnSaveKeys.UseVisualStyleBackColor = true;
+            this.btnSaveKeys.Click += new System.EventHandler(this.btnSaveKeys_Click);
+            // 
+            // btnGenerateKeys
+            // 
+            this.btnGenerateKeys.Location = new System.Drawing.Point(404, 221);
+            this.btnGenerateKeys.Name = "btnGenerateKeys";
+            this.btnGenerateKeys.Size = new System.Drawing.Size(182, 23);
+            this.btnGenerateKeys.TabIndex = 1;
+            this.btnGenerateKeys.Text = "Генерувати ключі";
+            this.btnGenerateKeys.UseVisualStyleBackColor = true;
+            this.btnGenerateKeys.Click += new System.EventHandler(this.btnGenerateKeys_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(404, 279);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Вихід";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(403, 58);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(183, 23);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Зашифрувати текст";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecryptFile
+            // 
+            this.btnDecryptFile.Location = new System.Drawing.Point(404, 164);
+            this.btnDecryptFile.Name = "btnDecryptFile";
+            this.btnDecryptFile.Size = new System.Drawing.Size(182, 23);
+            this.btnDecryptFile.TabIndex = 4;
+            this.btnDecryptFile.Text = "Розшифрувати файл";
+            this.btnDecryptFile.UseVisualStyleBackColor = true;
+            this.btnDecryptFile.Click += new System.EventHandler(this.btnDecryptFile_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(403, 87);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(183, 23);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Розшифрувати текст";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(486, 279);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Очистити";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEncryptFile
+            // 
+            this.btnEncryptFile.Location = new System.Drawing.Point(403, 132);
+            this.btnEncryptFile.Name = "btnEncryptFile";
+            this.btnEncryptFile.Size = new System.Drawing.Size(183, 23);
+            this.btnEncryptFile.TabIndex = 7;
+            this.btnEncryptFile.Text = "Зашифрувати файл";
+            this.btnEncryptFile.UseVisualStyleBackColor = true;
+            this.btnEncryptFile.Click += new System.EventHandler(this.btnEncryptFile_Click);
+            // 
+            // txtPublicKey
+            // 
+            this.txtPublicKey.Location = new System.Drawing.Point(28, 48);
+            this.txtPublicKey.Multiline = true;
+            this.txtPublicKey.Name = "txtPublicKey";
+            this.txtPublicKey.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPublicKey.Size = new System.Drawing.Size(350, 62);
+            this.txtPublicKey.TabIndex = 8;
+            // 
+            // txtPrivateKey
+            // 
+            this.txtPrivateKey.Location = new System.Drawing.Point(28, 132);
+            this.txtPrivateKey.Multiline = true;
+            this.txtPrivateKey.Name = "txtPrivateKey";
+            this.txtPrivateKey.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPrivateKey.Size = new System.Drawing.Size(350, 55);
+            this.txtPrivateKey.TabIndex = 9;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(31, 209);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessage.Size = new System.Drawing.Size(347, 85);
+            this.txtMessage.TabIndex = 10;
+            // 
+            // lblPublicKey
+            // 
+            this.lblPublicKey.AutoSize = true;
+            this.lblPublicKey.Location = new System.Drawing.Point(25, 29);
+            this.lblPublicKey.Name = "lblPublicKey";
+            this.lblPublicKey.Size = new System.Drawing.Size(144, 16);
+            this.lblPublicKey.TabIndex = 11;
+            this.lblPublicKey.Text = "Відкритий ключ (e, n):";
+            // 
+            // вса
+            // 
+            this.вса.AutoSize = true;
+            this.вса.Location = new System.Drawing.Point(28, 190);
+            this.вса.Name = "вса";
+            this.вса.Size = new System.Drawing.Size(179, 16);
+            this.вса.TabIndex = 12;
+            this.вса.Text = "Повідомлення/результат: ";
+            // 
+            // lblPrivateKey
+            // 
+            this.lblPrivateKey.AutoSize = true;
+            this.lblPrivateKey.Location = new System.Drawing.Point(28, 113);
+            this.lblPrivateKey.Name = "lblPrivateKey";
+            this.lblPrivateKey.Size = new System.Drawing.Size(141, 16);
+            this.lblPrivateKey.TabIndex = 13;
+            this.lblPrivateKey.Text = "Закритий ключ (d, n):";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(28, 315);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(163, 16);
+            this.lblStatus.TabIndex = 14;
+            this.lblStatus.Text = "Статус: очікування дій...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblPrivateKey);
+            this.Controls.Add(this.вса);
+            this.Controls.Add(this.lblPublicKey);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtPrivateKey);
+            this.Controls.Add(this.txtPublicKey);
+            this.Controls.Add(this.btnEncryptFile);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnDecryptFile);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGenerateKeys);
+            this.Controls.Add(this.btnSaveKeys);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaveKeys;
+        private System.Windows.Forms.Button btnGenerateKeys;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecryptFile;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEncryptFile;
+        private System.Windows.Forms.TextBox txtPublicKey;
+        private System.Windows.Forms.TextBox txtPrivateKey;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label lblPublicKey;
+        private System.Windows.Forms.Label вса;
+        private System.Windows.Forms.Label lblPrivateKey;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ lab_6/Form1.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Numerics;
+
+namespace lab_6
+{
+    public partial class Form1 : Form
+    {
+        private BigInteger n, e, d;
+        private bool keysGenerated = false;
+        private readonly RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        private readonly int keySize = 1024; // Розмір ключа в бітах
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigureUI();
+        }
+
+        private void ConfigureUI()
+        {
+            this.Text = "Асиметричне шифрування RSA (1024-біт)";
+            btnClear.Text = "Очистити";
+            btnExit.Text = "Вихід";
+            btnGenerateKeys.Text = "Генерувати ключі";
+            btnSaveKeys.Text = "Зберегти ключі";
+            btnEncrypt.Text = "Зашифрувати текст";
+            btnDecrypt.Text = "Розшифрувати текст";
+            btnEncryptFile.Text = "Зашифрувати файл";
+            btnDecryptFile.Text = "Розшифрувати файл";
+            lblStatus.Text = "Статус: очікування генерації ключів";
+            lblPublicKey.Text = "Відкритий ключ (e, n):";
+            lblPrivateKey.Text = "Закритий ключ (d, n):";
+        }
+
+        private void btnGenerateKeys_Click(object sender, EventArgs e)
+        {
+            var sw = Stopwatch.StartNew();
+
+            BigInteger p = GenerateLargePrime(keySize / 2);
+            BigInteger q = GenerateLargePrime(keySize / 2);
+
+            n = p * q;
+            BigInteger phi = (p - 1) * (q - 1);
+            this.e = 65537; // Використовуємо поле класу, а не параметр події
+            d = ModInverse(this.e, phi);
+
+            sw.Stop();
+
+            txtPublicKey.Text = $"e: {this.e}\nn: {n}";
+            txtPrivateKey.Text = $"d: {d}\nn: {n}";
+            keysGenerated = true;
+
+            lblStatus.Text = $"Статус: ключі згенеровані за {sw.ElapsedMilliseconds} мс";
+            MessageBox.Show($"1024-бітні ключі успішно згенеровані!\nЧас генерації: {sw.ElapsedMilliseconds} мс",
+                "Інформація", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private BigInteger GenerateLargePrime(int bits)
+        {
+            BigInteger number;
+            do
+            {
+                number = GenerateRandomBigInteger(bits);
+            } while (!IsProbablePrime(number, 10));
+
+            return number;
+        }
+
+        private BigInteger GenerateRandomBigInteger(int bits)
+        {
+            byte[] bytes = new byte[bits / 8];
+            rng.GetBytes(bytes);
+            bytes[bytes.Length - 1] &= 0x7F;
+            return new BigInteger(bytes);
+        }
+
+        private bool IsProbablePrime(BigInteger n, int k)
+        {
+            if (n == 2 || n == 3) return true;
+            if (n < 2 || n % 2 == 0) return false;
+
+            BigInteger d = n - 1;
+            int s = 0;
+
+            while (d % 2 == 0)
+            {
+                d /= 2;
+                s++;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                byte[] bytes = new byte[n.ToByteArray().Length];
+                BigInteger a;
+                do
+                {
+                    rng.GetBytes(bytes);
+                    a = new BigInteger(bytes);
+                } while (a < 2 || a >= n - 2);
+
+                BigInteger x = BigInteger.ModPow(a, d, n);
+                if (x == 1 || x == n - 1) continue;
+
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == 1) return false;
+                    if (x == n - 1) break;
+                }
+
+                if (x != n - 1) return false;
+            }
+
+            return true;
+        }
+
+        private BigInteger ModInverse(BigInteger a, BigInteger m)
+        {
+            BigInteger m0 = m;
+            BigInteger y = 0, x = 1;
+
+            if (m == 1) return 0;
+
+            while (a > 1)
+            {
+                BigInteger q = a / m;
+                BigInteger t = m;
+
+                m = a % m;
+                a = t;
+                t = y;
+
+                y = x - q * y;
+                x = t;
+            }
+
+            if (x < 0) x += m0;
+
+            return x;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs eventArgs)
+        {
+            if (!keysGenerated)
+            {
+                MessageBox.Show("Спочатку згенеруйте ключі!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                MessageBox.Show("Введіть текст для шифрування!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                var sw = Stopwatch.StartNew();
+                string plainText = txtMessage.Text;
+                byte[] bytes = Encoding.UTF8.GetBytes(plainText);
+                int blockSize = (keySize / 8) - 12;
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    for (int i = 0; i < bytes.Length; i += blockSize)
+                    {
+                        int length = Math.Min(blockSize, bytes.Length - i);
+                        byte[] block = new byte[length];
+                        Array.Copy(bytes, i, block, 0, length);
+
+                        BigInteger m = new BigInteger(block);
+                        if (m >= n)
+                        {
+                            MessageBox.Show("Помилка: дані занадто великі для обраного ключа", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        BigInteger c = BigInteger.ModPow(m, e, n);
+                        byte[] encryptedBlock = c.ToByteArray();
+                        ms.Write(BitConverter.GetBytes(encryptedBlock.Length), 0, 4);
+                        ms.Write(encryptedBlock, 0, encryptedBlock.Length);
+                    }
+
+                    txtMessage.Text = Convert.ToBase64String(ms.ToArray());
+                    sw.Stop();
+                    lblStatus.Text = $"Статус: текст зашифровано за {sw.ElapsedMilliseconds} мс";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при шифруванні: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs eventArgs)
+        {
+            if (!keysGenerated)
+            {
+                MessageBox.Show("Спочатку згенеруйте ключі!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                MessageBox.Show("Введіть зашифрований текст!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                var sw = Stopwatch.StartNew();
+                byte[] encryptedData = Convert.FromBase64String(txtMessage.Text);
+
+                using (MemoryStream ms = new MemoryStream(encryptedData))
+                using (MemoryStream result = new MemoryStream())
+                {
+                    byte[] lengthBytes = new byte[4];
+
+                    while (ms.Position < ms.Length)
+                    {
+                        ms.Read(lengthBytes, 0, 4);
+                        int blockLength = BitConverter.ToInt32(lengthBytes, 0);
+                        byte[] encryptedBlock = new byte[blockLength];
+                        ms.Read(encryptedBlock, 0, blockLength);
+
+                        BigInteger c = new BigInteger(encryptedBlock);
+                        BigInteger m = BigInteger.ModPow(c, d, n);
+                        byte[] decryptedBlock = m.ToByteArray();
+
+                        result.Write(decryptedBlock, 0, decryptedBlock.Length);
+                    }
+
+                    txtMessage.Text = Encoding.UTF8.GetString(result.ToArray());
+                    sw.Stop();
+                    lblStatus.Text = $"Статус: текст розшифровано за {sw.ElapsedMilliseconds} мс";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Помилка при дешифруванні: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEncryptFile_Click(object sender, EventArgs e)
+        {
+            if (!keysGenerated)
+            {
+                MessageBox.Show("Спочатку згенеруйте ключі!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Зашифровані файли (*.rsa)|*.rsa";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var sw = Stopwatch.StartNew();
+                        byte[] fileData = File.ReadAllBytes(openFileDialog.FileName);
+                        int blockSize = (keySize / 8) - 12;
+
+                        using (FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                        {
+                            for (int i = 0; i < fileData.Length; i += blockSize)
+                            {
+                                int length = Math.Min(blockSize, fileData.Length - i);
+                                byte[] block = new byte[length];
+                                Array.Copy(fileData, i, block, 0, length);
+
+                                BigInteger m = new BigInteger(block);
+                                if (m >= n)
+                                {
+                                    MessageBox.Show("Помилка: файл занадто великий для обраного ключа", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                }
+
+                                BigInteger c = BigInteger.ModPow(m, this.e, n);
+                                byte[] encryptedBlock = c.ToByteArray();
+                                fs.Write(BitConverter.GetBytes(encryptedBlock.Length), 0, 4);
+                                fs.Write(encryptedBlock, 0, encryptedBlock.Length);
+                            }
+                        }
+
+                        sw.Stop();
+                        lblStatus.Text = $"Статус: файл зашифровано за {sw.ElapsedMilliseconds} мс";
+                        MessageBox.Show($"Файл успішно зашифровано!\nЧас шифрування: {sw.ElapsedMilliseconds} мс",
+                            "Інформація", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Помилка при шифруванні файлу: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnDecryptFile_Click(object sender, EventArgs e)
+        {
+            if (!keysGenerated)
+            {
+                MessageBox.Show("Спочатку згенеруйте ключі!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Зашифровані файли (*.rsa)|*.rsa";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var sw = Stopwatch.StartNew();
+                        using (FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open))
+                        using (FileStream result = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                        {
+                            byte[] lengthBytes = new byte[4];
+
+                            while (fs.Position < fs.Length)
+                            {
+                                fs.Read(lengthBytes, 0, 4);
+                                int blockLength = BitConverter.ToInt32(lengthBytes, 0);
+                                byte[] encryptedBlock = new byte[blockLength];
+                                fs.Read(encryptedBlock, 0, blockLength);
+
+                                BigInteger c = new BigInteger(encryptedBlock);
+                                BigInteger m = BigInteger.ModPow(c, this.d, n);
+                                byte[] decryptedBlock = m.ToByteArray();
+
+                                result.Write(decryptedBlock, 0, decryptedBlock.Length);
+                            }
+                        }
+
+                        sw.Stop();
+                        lblStatus.Text = $"Статус: файл розшифровано за {sw.ElapsedMilliseconds} мс";
+                        MessageBox.Show($"Файл успішно розшифровано!\nЧас дешифрування: {sw.ElapsedMilliseconds} мс",
+                            "Інформація", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Помилка при дешифруванні файлу: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnSaveKeys_Click(object sender, EventArgs e)
+        {
+            if (!keysGenerated)
+            {
+                MessageBox.Show("Спочатку згенеруйте ключі!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстові файли (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName,
+                        $"Відкритий ключ (e, n):\n{this.e}\n{n}\n\nЗакритий ключ (d, n):\n{d}\n{n}");
+                    MessageBox.Show("Ключі успішно збережено!", "Інформація", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка при збереженні ключів: {ex.Message}", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMessage.Clear();
+            lblStatus.Text = "Статус: поля очищено";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
